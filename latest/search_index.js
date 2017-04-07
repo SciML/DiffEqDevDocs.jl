229fa4375f14f3890b8d36f5e050d9dfed1f063e
autodocs <autodocs>
2017-04-07 07:03:22 +0000

    build based on 941730c

--- latest/search_index.js
+++ latest/search_index.js
@@ -170,10 +170,34 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "contributing/parameters.html#",
-    "page": "-",
-    "title": "-",
+    "page": "The Parameters Interface",
+    "title": "The Parameters Interface",
     "category": "page",
     "text": ""
+},
+
+{
+    "location": "contributing/parameters.html#The-Parameters-Interface-1",
+    "page": "The Parameters Interface",
+    "title": "The Parameters Interface",
+    "category": "section",
+    "text": "The parameters interface allows for parameters in functions to be added and accessed by the various routines which needs parameters, and allows them to be \"buried\" in the routines which need to not have explicit parameters."
+},
+
+{
+    "location": "contributing/parameters.html#The-Functions-on-Parameterized-Functions-1",
+    "page": "The Parameters Interface",
+    "title": "The Functions on Parameterized Functions",
+    "category": "section",
+    "text": "param_values(f) # An array of the parameters in the function\nnum_params(f) # The number of parameters in the function"
+},
+
+{
+    "location": "contributing/parameters.html#Building-a-New-Problem-with-New-Parameters-1",
+    "page": "The Parameters Interface",
+    "title": "Building a New Problem with New Parameters",
+    "category": "section",
+    "text": "From a DEProblem, the functionproblem_new_parameters(prob::DEProblem,p)can be used to build a new problem type which uses the parameter vector p. For problems which have parameters in multiple places, such as a SDEProblem or a MonteCarloProblem, this p is split between these locations using num_params to find out how much of p goes to each location."
 },
 
 {
